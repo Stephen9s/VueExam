e2b7b199e0ad178c644258ffd8ac202f8304e980
TheStoof <neyens.s@gmail.com>
2018-05-06 16:04:10 -0400

    Removed all references to Laravel. Using Fat Free for basic routing.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,21 +1,7 @@
 <?
 
 namespace App\Http\Controllers;
-use CapsuleManager\Wrapper\Manager as Capsule;
 
 class Controller {
-    protected $capsule;
-    protected $connectionPDO;
 
-    public function __construct() {
-        // Don't care if this gets checked in; defaults for MySQL.
-        $this->connectionPDO = new \PDO("mysql:host=127.0.0.1;dbname=vueexam;", "root", "password");
-        $this->capsule = new Capsule($this->connectionPDO);
-
-        // Make this Capsule instance available globally via static methods
-        $this->capsule->setAsGlobal();
-
-        // Setup the Eloquent ORM
-        $this->capsule->bootEloquent();
-    }
 }
--- web/routes.php
+++ web/routes.php
@@ -1,4 +1,3 @@
 <?
 
 $f3->route('GET /', 'App\Http\Controllers\Home->index');
-$f3->route('GET /jsonData/@id', 'App\Http\Controllers\Exams->get');
